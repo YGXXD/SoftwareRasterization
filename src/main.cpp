4a3548cf8552d21afa110d6f833612159a38d4a5
chenqiudu <chenqiudu@kuaishou.com>
2023-11-06 13:10:06 +0800

    clear header

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,11 @@
-#include "SDL.h"
+#include <iostream>
+#include <SDL.h>
 #include "math/MathLibrary.h"
 #include "math/Matrix4X4.h"
 #include "math/Vector3.h"
 #include "render/DepthStencilBuffer.h"
 #include "render/RenderTargetBuffer.h"
 #include "render/DrawLibrary.h"
-#include <SDL_events.h>
-#include <SDL_keyboard.h>
-#include <SDL_keycode.h>
-#include <SDL_pixels.h>
-#include <SDL_rect.h>
-#include <SDL_timer.h>
-#include <SDL_video.h>
-#include <array>
-#include <iostream>
-
-const int w = 1280, h = 720;
-SDL_Window* window;
-RenderTargetBuffer* render_buffer;
-DepthStencilBuffer* depth_stencil_buffer;
-Matrix4X4* CameraMatrix;
-
-float R = 15;
-float A = 0;
-float B = 0;
 
 struct RenderItem
 {
@@ -32,11 +14,17 @@ struct RenderItem
 };
 GeometryInfo RenderItem::Box = Geometry::GetBoxGeometryData();
 
+const int w = 1280, h = 720;
+SDL_Window* window;
 RenderItem Item = {Vector3(0.f, 0.f, 0.f)};
+RenderTargetBuffer* render_buffer;
+DepthStencilBuffer* depth_stencil_buffer;
+Matrix4X4* CameraMatrix;
+
+float Raduis = 15;
 
 void Update(float DeltaTime);
 void Render(float DeltaTime);
-void Test();
 
 int main()
 {	
@@ -59,11 +47,11 @@ int main()
 			{
 				if(event.key.keysym.sym == SDLK_i)
 				{
-					R = Math::Clamp<float>(10, 20, R + 1); 
+					Raduis = Math::Clamp<float>(10, 20, Raduis + 1); 
 				}
 				if(event.key.keysym.sym == SDLK_k)
 				{
-					R = Math::Clamp<float>(10, 20, R - 1); 
+					Raduis = Math::Clamp<float>(10, 20, Raduis - 1); 
 				}
 			}
 		}
@@ -82,18 +70,6 @@ int main()
 		Render(DeltaTime);
 	}
 
-	/*
-	Uint32* canval_ptr = (Uint32*)canval->pixels;
-	for(int y = 0; y < h; y++)
-	{
-		for(int x = 0;x < w; x++)
-		{
-			canval_ptr[x + y * w] = render_buffer->GetBuffer(x, h - 1 - y); 
-			std::cout << std::hex << canval_ptr[x + y * w] << " ";
-		}
-		std::cout << std::endl;
-	}
-	*/
 	delete render_buffer;
 	delete depth_stencil_buffer;
 	delete CameraMatrix;
@@ -105,13 +81,16 @@ int main()
 
 void Update(float DeltaTime) 
 {
+	static float A = 0;
+	static float B = 0;
+
 	B += DeltaTime;
 	A += DeltaTime;
 
 	float Pitch = Math::Sin(A) * 1.5707963; 
 	float Yaw = B;
 
-	Vector3 EyePosition(R * Math::Cos(Pitch) * Math::Cos(Yaw), R * Math::Cos(Pitch) * Math::Sin(Yaw), R * Math::Sin(Pitch));
+	Vector3 EyePosition(Raduis * Math::Cos(Pitch) * Math::Cos(Yaw), Raduis * Math::Cos(Pitch) * Math::Sin(Yaw), Raduis * Math::Sin(Pitch));
 	Vector3 FocusPosition(0, 0, 0);
 	Vector3 UpDirection(0, 0, 1);
 
@@ -147,52 +126,4 @@ void Render(float DeltaTime)
 		}
 	}
 	SDL_UpdateWindowSurface(window);
-}
-
-void Test()
-{
-	Matrix4X4 M = {2,1,4,5,
-				   1,2,1,6,
-				   2,1,2,1,
-				   5,3,0,2};
-	std::cout << "原矩阵" << std::endl;
-	std::cout << M.m11 << "," << M.m12 << "," << M.m13 << "," << M.m14 << std::endl;	
-	std::cout << M.m21 << "," << M.m22 << "," << M.m23 << "," << M.m24 << std::endl;	
-	std::cout << M.m31 << "," << M.m32 << "," << M.m33 << "," << M.m34 << std::endl;	
-	std::cout << M.m41 << "," << M.m42 << "," << M.m43 << "," << M.m44 << std::endl;	
-
-	std::cout << "---------------------------------" << std::endl;
-
-	Matrix4X4 MI = Math::MatrixInverse(M); // Math::MatrixLookAt(Vector3(10,0,10), Vector3(0,0,0), Vector3(0,1,0));
-	
-	std::cout << "逆矩阵" << std::endl;
-	std::cout << MI.m11 << "," << MI.m12 << "," << MI.m13 << "," << MI.m14 << std::endl;	
-	std::cout << MI.m21 << "," << MI.m22 << "," << MI.m23 << "," << MI.m24 << std::endl;	
-	std::cout << MI.m31 << "," << MI.m32 << "," << MI.m33 << "," << MI.m34 << std::endl;	
-	std::cout << MI.m41 << "," << MI.m42 << "," << MI.m43 << "," << MI.m44 << std::endl;	
-
-	std::cout << "---------------------------------" << std::endl;
-
-	MI = MI * M;	
-
-	std::cout << "原*逆矩阵" << std::endl;
-	std::cout << MI.m11 << "," << MI.m12 << "," << MI.m13 << "," << MI.m14 << std::endl;	
-	std::cout << MI.m21 << "," << MI.m22 << "," << MI.m23 << "," << MI.m24 << std::endl;	
-	std::cout << MI.m31 << "," << MI.m32 << "," << MI.m33 << "," << MI.m34 << std::endl;	
-	std::cout << MI.m41 << "," << MI.m42 << "," << MI.m43 << "," << MI.m44 << std::endl;	
-
-	Pixel p1 =  {90,30,0,255,0,255};
-	Pixel p2 =  {30,80,0,0,255,255};
-	Pixel p3 =  {10,40,255,0,0,255};
-	Draw2D::DrawTriangleByBarycentic(render_buffer, &p1, &p2, &p3);
-
-	Pixel p4 =  {400,400,0,255,0,255};
-	Pixel p5 =  {300,200,0,0,255,255};
-	Pixel p6 =  {20,100,255,0,0,255};
-	Draw2D::DrawTriangleByBarycentic(render_buffer, &p4, &p5, &p6);
-
-	Pixel p7 =  {300,300,0,255,0,255};
-	Pixel p8 =  {100,100,0,0,255,255};
-	Pixel p9 =  {600,60,255,0,0,255};
-	Draw2D::DrawTriangleByBarycentic(render_buffer, &p7, &p8, &p9);
 }
